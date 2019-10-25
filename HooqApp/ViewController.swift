93c00498eaf77162abdc023a84c45d9c27abe684
Balaji Galave <balaji.galave@globant.com>
2019-10-25 18:34:42 +0530

    Initial Commit

---
+++ HooqApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HooqApp
+//
+//  Created by Balaji Galave on 10/25/19.
+//  Copyright © 2019 Balaji Galave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
