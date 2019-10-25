a3a10a24265c226246b461424199841817a84d68
balajigalave <balaji.sonora@gmail.com>
2019-10-25 19:29:15 +0530

    adds basic architecure setup

---
+++ HooqApp/Presentation/Common/BaseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BaseViewController.swift
+//  HooqApp
+//
+//  Created by Balaji Galave on 25/10/19.
+//  Copyright © 2019 Balaji Galave. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ HooqApp/Presentation/MovieList/MovieListPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  MovieListPresenter.swift
+//  HooqApp
+//
+//  Created by Balaji Galave on 25/10/19.
+//  Copyright © 2019 Balaji Galave. All rights reserved.
+//
+
+import Foundation
